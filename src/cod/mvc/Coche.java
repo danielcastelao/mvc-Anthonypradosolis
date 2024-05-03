0890d3471b7bebe1ce83037a29423485575e4090
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-03 09:57:17 +0200

    Creacion del constructor de la clase Coche

--- src/cod/mvc/Coche.java
+++ src/cod/mvc/Coche.java
@@ -4,4 +4,10 @@ public class Coche {
     private String matricula;
     private String modelo;
     private int velocidad;
+
+    public Coche(String matricula, String modelo, int velocidad) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.velocidad = velocidad;
+    }
 }
