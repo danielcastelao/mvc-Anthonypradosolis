fec507a8d0b61991b4d11c4d7aec310af7469657
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-03 10:10:37 +0200

    Creacion del metodo crearCoche

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -5,4 +5,11 @@ import java.util.ArrayList;
 public class Model {
     ArrayList<Coche> parking = new ArrayList<>();
 
+    public Coche crearCoche(String matricula, String modelo, int velocidad){
+        Coche coche = null;
+        coche.setMatricula(matricula);
+        coche.setModelo(modelo);
+        coche.setVelocidad(velocidad);
+        return coche;
+    }
 }
