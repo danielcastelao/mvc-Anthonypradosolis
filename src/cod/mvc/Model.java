3acaeda2a83ce6d9085f0bf42e27fe8b879ac403
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-04 13:11:05 +0200

    Creamos la variable de coche, y modificamos el metodo crearCoche

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 
 public class Model {
     ArrayList<Coche> parking = new ArrayList<>();
+    Coche coche;
 
     public Coche crearCoche(String matricula, String modelo, int velocidad){
-        Coche coche = null;
-        coche.setMatricula(matricula);
-        coche.setModelo(modelo);
-        coche.setVelocidad(velocidad);
-        parking.add(new Coche(coche.getMatricula(), coche.getModelo(),coche.getVelocidad()));
+        coche = new Coche(matricula,modelo,velocidad);
+        parking.add(coche);
         return coche;
     }
 }
