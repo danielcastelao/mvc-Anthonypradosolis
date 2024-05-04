edb7130c4a586f7964b874547790caa36f5d12c0
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-04 13:15:33 +0200

    Creamos el metodo muestraVelocidad

--- src/cod/mvc/View.java
+++ src/cod/mvc/View.java
@@ -6,5 +6,12 @@ public class View {
         public View(Model model) {
             this.model = model;
         }
-
+        public void muestraVelocidad(String matricula) {
+            int velocidad = model.getVelocidad(matricula);
+            if (velocidad != -1) {
+                System.out.println("La velocidad del coche con matrícula " + matricula + " es: " + velocidad);
+            } else {
+                System.out.println("No se encontró un coche con la matrícula " + matricula);
+            }
+        }
 }
