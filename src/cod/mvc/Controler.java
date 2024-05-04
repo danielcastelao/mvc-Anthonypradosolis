c7494daeb58bcddedecea30f0543ccb1a3504913
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-04 13:18:06 +0200

    Hacemos que el programa funcione

--- src/cod/mvc/Controler.java
+++ src/cod/mvc/Controler.java
@@ -2,6 +2,16 @@ package cod.mvc;
 
 public class Controler {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Model model = new Model();
+
+        model.crearCoche("1111-ABC", "Toyota", 120);
+
+        View view = new View(model);
+
+        view.muestraVelocidad("1111-ABC");
+
+        model.cambiarVelocidad("1111-ABC", 150);
+
+        view.muestraVelocidad("1111-ABC");
     }
 }
