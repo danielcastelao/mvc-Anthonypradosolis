f524c208dcd6cc16e7cd439e22bc8d5c8c540957
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-06 09:05:35 +0200

    Modificacion de los metodos a modo static

--- src/cod/mvc/Model.java
+++ src/cod/mvc/Model.java
@@ -3,15 +3,15 @@ package cod.mvc;
 import java.util.ArrayList;
 
 public class Model {
-    ArrayList<Coche> parking = new ArrayList<>();
-    Coche coche;
+    static ArrayList<Coche> parking = new ArrayList<>();
+    static Coche coche;
 
-    public Coche crearCoche(String matricula, String modelo, int velocidad){
+    public static Coche crearCoche(String matricula, String modelo, int velocidad){
         coche = new Coche(matricula,modelo,velocidad);
         parking.add(coche);
         return coche;
     }
-    public Coche getCoche(String matricula) {
+    public static Coche getCoche(String matricula) {
         for (Coche coche : parking) {
             if (coche.getMatricula().equals(matricula)) {
                 return coche;
@@ -19,13 +19,13 @@ public class Model {
         }
         return null;
     }
-    public void cambiarVelocidad(String matricula, int nuevaVelocidad) {
+    public static void cambiarVelocidad(String matricula, int nuevaVelocidad) {
         Coche coche = getCoche(matricula);
         if (coche != null) {
             coche.setVelocidad(nuevaVelocidad);
         }
     }
-    public int getVelocidad(String matricula) {
+    public static int getVelocidad(String matricula) {
         Coche coche = getCoche(matricula);
         if (coche != null) {
             return coche.getVelocidad();
