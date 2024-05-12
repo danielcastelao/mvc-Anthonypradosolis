89ce560d50d6ca1012dc7cbdb270228a6f9ffbcb
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-12 14:10:29 +0200

    Comentamos todo ya que todavia no tengo que realizar este paso

--- src/cod/mvc/Controler.java
+++ src/cod/mvc/Controler.java
@@ -6,36 +6,40 @@ import java.awt.event.ActionListener;
 public class Controler {
     public static void main(String[] args) {
         Model model = new Model();
-        View view = new View(model);
+//        View view = new View(model);
 
         Coche coche = model.crearCoche("1111-ABC", "Toyota", 120);
+//
+//        ActionListener acelerarHandler = new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                int velocidadActual = coche.getVelocidad();
+//                coche.setVelocidad(velocidadActual + 10);
+//                view.printVelocidad(coche);
+//            }
+//        };
 
-        ActionListener acelerarHandler = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int velocidadActual = coche.getVelocidad();
-                coche.setVelocidad(velocidadActual + 10);
-                view.displayVelocity(coche);
-            }
-        };
+//        ActionListener frenarHandler = new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                int velocidadActual = coche.getVelocidad();
+//                if (velocidadActual >= 10) {
+//                    coche.setVelocidad(velocidadActual - 10);
+//                } else {
+//                    coche.setVelocidad(0);
+//                }
+//                view.printVelocidad(coche);
+//            }
+//        };
+//
+//        view.crearMandos(coche, acelerarHandler, frenarHandler);
+//
+//        view.printVelocidad(coche);
+//        model.cambiarVelocidad("1111-ABC", 150);
+//        view.printVelocidad(coche);
 
-        ActionListener frenarHandler = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int velocidadActual = coche.getVelocidad();
-                if (velocidadActual >= 10) {
-                    coche.setVelocidad(velocidadActual - 10);
-                } else {
-                    coche.setVelocidad(0);
-                }
-                view.displayVelocity(coche);
-            }
-        };
 
-        view.crearMandos(coche, acelerarHandler, frenarHandler);
 
-        view.displayVelocity(coche);
-        model.cambiarVelocidad("1111-ABC", 150);
-        view.displayVelocity(coche);
+
     }
 }
