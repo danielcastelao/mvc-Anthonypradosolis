48d145173d10916912e62296950580ec2d485aa8
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-13 09:22:32 +0200

    Refactor de todas las clases

--- src/cod/mvc/view/View.java
+++ src/cod/mvc/view/View.java
@@ -1,9 +1,6 @@
-package cod.mvc;
+package cod.mvc.view;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import cod.mvc.model.Coche;
 
 public class View {
 //    private Model model;
