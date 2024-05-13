19a8e2997c22d365f77ce7f2f01de45648a8a593
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-13 09:51:54 +0200

    creacion de metodos en la interfaz de Observer

--- src/cod/mvc/controller/Observer.java
+++ src/cod/mvc/controller/Observer.java
@@ -1,4 +1,11 @@
 package cod.mvc.controller;
+import cod.mvc.model.Coche;
 
+/**
+ * Interfaz Observer que implementarán los observadores
+
+ */
 public interface Observer {
+    // metodo que se ejecutará cuando el objeto observado cambie
+    void update(Coche arg);
 }
