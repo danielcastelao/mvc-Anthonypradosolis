ff5a8d366299d9775f48ec8ed06243a929be6978
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-30 22:20:39 +0200

    añadimos los getters y los setters

--- src/cod/mvc/model/Coche.java
+++ src/cod/mvc/model/Coche.java
@@ -11,5 +11,27 @@ public class Coche {
         this.velocidad = 0;
     }
 
+    public String getModelo() {
+        return modelo;
+    }
 
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Integer getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(Integer velocidad) {
+        this.velocidad = velocidad;
+    }
 }
