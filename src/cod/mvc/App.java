c8f0911c416a225d3056e5076059355b87914347
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-31 09:40:21 +0200

    creamos toda la funcionalidad del programa close #8

--- src/cod/mvc/App.java
+++ src/cod/mvc/App.java
@@ -1,18 +1,22 @@
 package cod.mvc;
 
 import cod.mvc.controller.Controller;
+import cod.mvc.model.Coche;
 import cod.mvc.model.Model;
 import cod.mvc.view.View;
+
+import java.util.Scanner;
 
 /**
  * Clase principal de la aplicación
  */
 public class App {
 
+    private static Controller miController = new Controller(Model.getInstance());
     public static void main(String[] args) {
         // Inicializamos la app
         // instanciamos el modelo
-        Model miModel = new Model();
+        Model miModel = Model.getInstance();
         // instanciamos el controlador
         // le pasamos el Model instanciado
         Controller miController = new Controller(miModel);
@@ -28,8 +32,27 @@ public class App {
         // otro cambio de velocidad
         // sobrepasando la velocidad máxima
         miController.cambiarVelocidad("HYU 4567", 150);
-        View.crearVentana();
-        System.out.println("[App] fin del main");
+//        View.crearVentana();
+//        System.out.println("[App] fin del main");
+        buscarYMostrarCoche();
+
+    }
+
+    /**
+     * Metodo para buscar y mostrar los datos de coche
+     */
+    public static void buscarYMostrarCoche(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce la matricula del coche:");
+        String matricula = scanner.nextLine();
+        Coche coche = miController.buscarYMostrarCoche(matricula);
+        if(coche!=null){
+            System.out.println("Matricula: "+coche.matricula);
+            System.out.println("Modelo:" + coche.modelo);
+            System.out.println("Velocidad:"+coche.velocidad);
+        } else{
+            System.out.println("No se encontro un coche con la matricula dada");
+        }
 
     }
 }
